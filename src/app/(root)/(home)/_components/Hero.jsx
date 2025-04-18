71d6990eb14adc9c0202cba34d114bf44a985259
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-18 23:31:40 +0700

    core structure and classes and text stuffs, nice

--- src/app/(root)/(home)/_components/Hero.jsx
+++ src/app/(root)/(home)/_components/Hero.jsx
@@ -1,8 +1,23 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './hero.module.css'
 
 const Hero = () => {
+  // State to manage the checkbox's checked state
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Function to toggle the checkbox
+  const handleCheckboxToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
+  // Function to prevent checkbox toggle when clicking inside the help section
+  const handleHelpClick = (e) => {
+    e.stopPropagation(); // Stop click propagation for help section
+  };
+
   return (
     <div className={`${styles.fontController} poop`}>
 
@@ -30,8 +45,6 @@ const Hero = () => {
           </p>
         </div>
 
-  
-
         <BulletList
           bullets={[
             { icon: '/white-round-bullet.svg', text: '100% custom & unique', alt: 'Star' },
@@ -49,6 +62,33 @@ const Hero = () => {
           ]}
         />
 
+        <div className={styles.upsellWrapper} onClick={handleCheckboxToggle}>
+          <input 
+            type="checkbox" 
+            className={styles.upsellCheckBox} 
+            checked={isChecked} // Bind state to the checkbox
+            onChange={handleCheckboxToggle} // Handle change event
+          />
+          <h6 className={styles.upsellText}>
+            Add Customizable UI
+            <span className={styles.upsellPriceText}> - $49</span>
+          </h6>
+          <div 
+            className={styles.upsellHelpWrapper}
+            onClick={handleHelpClick}  // Prevent checkbox toggle when clicking inside the help icon
+          >
+            <Image src="/package/help-icon.svg" alt="Help" className={styles.upsellHelpIcon} width={8} height={8} />
+          </div>
+        </div>
+
+        <button className={`${styles.atcBtn} big-btn full bold`}>
+          <Image src="/magic.svg" alt="Icon" width={18} height={18} />
+          <h6>Add To Cart</h6>
+          <div className={`btn-divider`}></div>
+          <h6 className={styles.atcPrice}>$149</h6>
+        </button>
+
+        <h6 className={styles.disclaimerText}>* Full satisfaction guarantee included.</h6>
 
       </div>
 
@@ -57,17 +97,11 @@ const Hero = () => {
         Streamline your brand and empower faster go-to-market by leveraging this and this...
       </p>
 
-      <button className="big-btn">
-        <Image src="/package/arrow.svg" alt="Arrow" className="arrow" width={16} height={16} />
-        <h5 style={{fontWeight: '700'}}>Get started</h5>
-      </button>
-
     </div>
   )
 }
 
 export default Hero
-
 
 const BulletList = ({ bullets = [], wrapperClass = '', itemClass = '', iconClass = '', textClass = '' }) => {
   return (
