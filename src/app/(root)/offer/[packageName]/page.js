3689f6d4b6e8e12f3b31ce4663891850b570af0b
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-22 03:08:34 +0700

    forgot to add to the product page LOL

--- src/app/(root)/offer/[packageName]/page.js
+++ src/app/(root)/offer/[packageName]/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./_components/Hero";
 import HowItWorks from "./_components/HowItWorks";
 import CTA from "@/global/components/CTA";
+import Results from "@/global/components/Results";
 
 export default async function Page( { params }) {
     const { packageName } = await params;
@@ -9,6 +10,7 @@ export default async function Page( { params }) {
     <>
       <Hero />
       <HowItWorks />
+      <Results />
       <CTA />
     </>
   );
