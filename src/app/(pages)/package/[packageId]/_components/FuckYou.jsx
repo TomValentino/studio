a0d0c55be55064309dd9f5f19fa390fabd0e75bc
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-25 22:35:08 +0700

    changes

---
+++ src/app/(pages)/package/[packageId]/_components/FuckYou.jsx
@@ -0,0 +1,55 @@
+import { handleFetchAllProducts } from "@/lib/client";
+import { Image as IKImage } from '@imagekit/next';
+import AtnBtn from "./AtnBtn";
+import { StripeWrapper } from "./Checkout";
+import { BenefitItem } from "./GlobalComponents";
+import styles from '../page.module.css'
+
+const FuckYou = async ({params}) => {
+    const products = await handleFetchAllProducts();
+    const { packageId } = await params;
+    const product = products.find((p) => p.id === packageId);
+  
+    if (!product) return <div>Product not found</div>; 
+    const components = {
+      'benefit-item': BenefitItem,
+    };
+  
+    return (
+      <div id={styles.topWrapper}>
+        <div id={styles.left}>
+          <h1 id={styles.topTitle}>{product.title}</h1>
+          <p>{product.description}</p>
+  
+          <IKImage
+            className={styles.topImages}
+            src={product.images[0].src}
+            width={1000}
+            height={740}
+            alt={product.title}
+          />
+  
+          {product.sections.map((section, index) => {
+            const { componentId, props = {} } = section;
+            const Section = components[componentId];
+            if (!Section) return null;
+            return <Section key={index} {...props} />;
+          })}
+        </div>
+        <div id={styles.right}>
+          <div id={styles.rightTopFlex}>
+            <h6 id={styles.rightTitle}>{product.name}</h6>
+            <div id={styles.rightPriceFlex}>
+              <h6 id={styles.rightPriceRRP}>${product.versions[0].priceRRP}</h6>
+              <h6 id={styles.rightPrice}>${product.versions[0].price}</h6>
+            </div>
+          </div>
+          <IKImage id={styles.rightIcon} src={product.icon} width={57} height={57} alt="" />
+          <AtnBtn product={product} />
+          <StripeWrapper />
+        </div>
+      </div>
+    );
+  }
+
+  export default FuckYou
--- src/app/(pages)/package/[packageId]/page.js
+++ src/app/(pages)/package/[packageId]/page.js
@@ -1,10 +1,6 @@
-import { Image as IKImage } from '@imagekit/next';
-import { BenefitItem } from './_components/GlobalComponents';
-import styles from './page.module.css';
-import AtnBtn from './_components/AtnBtn';
-import CheckoutForm, { StripeWrapper } from './_components/Checkout';
-import { handleFetchAllProducts } from '@/lib/client';
+
 import Footer from '@/global/components/Footer';
+import FuckYou from './_components/FuckYou';
 
 
 
@@ -13,56 +9,9 @@ const ProductPageContent = async ( { params }) => {
 
   return (
     <>
-    <FuckYou  params={params} />
+    <FuckYou params={params} />
     <Footer />
     </>
   )
 };
 export default ProductPageContent;
-
-const FuckYou = async ({params}) => {
-  const products = await handleFetchAllProducts();
-  const { packageId } = await params;
-  const product = products.find((p) => p.id === packageId);
-
-  if (!product) return <div>Product not found</div>; 
-  const components = {
-    'benefit-item': BenefitItem,
-  };
-
-  return (
-    <div id={styles.topWrapper}>
-      <div id={styles.left}>
-        <h1 id={styles.topTitle}>{product.title}</h1>
-        <p>{product.description}</p>
-
-        <IKImage
-          className={styles.topImages}
-          src={product.images[0].src}
-          width={1000}
-          height={740}
-          alt={product.title}
-        />
-
-        {product.sections.map((section, index) => {
-          const { componentId, props = {} } = section;
-          const Section = components[componentId];
-          if (!Section) return null;
-          return <Section key={index} {...props} />;
-        })}
-      </div>
-      <div id={styles.right}>
-        <div id={styles.rightTopFlex}>
-          <h6 id={styles.rightTitle}>{product.name}</h6>
-          <div id={styles.rightPriceFlex}>
-            <h6 id={styles.rightPriceRRP}>${product.versions[0].priceRRP}</h6>
-            <h6 id={styles.rightPrice}>${product.versions[0].price}</h6>
-          </div>
-        </div>
-        <IKImage id={styles.rightIcon} src={product.icon} width={57} height={57} alt="" />
-        <AtnBtn product={product} />
-        <StripeWrapper />
-      </div>
-    </div>
-  );
-}
