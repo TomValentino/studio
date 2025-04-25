943a966d24766e59e3f69180ccd20785abc26370
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-25 19:31:36 +0700

    true

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['ik.imagekit.io'], // Add ImageKit's domain to the list
     },
+    experimental: {
+      scrollRestoration: true,
+    }
   };
   
   export default nextConfig;
