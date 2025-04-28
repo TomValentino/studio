32983d4c3ba8fe2bcd2673e84fdd7c957ea84617
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-28 23:41:01 +0700

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     domains: ['ik.imagekit.io'], // Allow external images from ImageKit
   },
-  experimental: {
-    scrollRestoration: true, // Enables browser scroll restoration for App Router
-  },
+  // experimental: {
+  //   scrollRestoration: true, // Enables browser scroll restoration for App Router
+  // },
 };
 
 export default nextConfig;
