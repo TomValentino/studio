21869e9c56f3fcc9bbcc98ce3e012af8880a780e
Tom Valentino <tommyvalentino420@gmail.com>
2025-04-29 00:30:29 +0700

    df

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['ik.imagekit.io'], // Allow external images from ImageKit
   },
+  trailingSlash: true, // supposed to go to top of page with link shit
   // experimental: {
   //   scrollRestoration: true, // Enables browser scroll restoration for App Router
   // },
